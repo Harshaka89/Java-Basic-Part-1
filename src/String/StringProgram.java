644138ee90143c88073b9cf539ea5d9055864abc
“[Harshaka89]” <[buddhinathvitharana@gmail.com]>
2022-05-15 21:21:15 +0200

    Initial commit

---
+++ src/String/StringProgram.java
@@ -0,0 +1,21 @@
+package String;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Fill in the blank ->");
+		System.out.println("My first Name is _____and my last name is ______");
+		
+		String FirstName = "Harshka";
+		String LastName = "Waththegedara";
+		
+		System.out.println("Harshka");
+		System.out.println("Waththegedara");
+		
+		System.out.println("My first Name is "+ FirstName +"and my last name is "+LastName + "Thank You!");
+
+	}
+
+}
---
+++ src/home.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class home {
+	public static void main(String[] args) {
+	System.out.println("Harshka");	
+	
+	Scanner scan =new Scanner(System.in);
+	
+	System.out.println("Java lessen 1");
+	String No1 =scan.nextLine();
+	
+	System.out.println(No1);
+	System.out.println("2");
+	System.out.println("3");
+	System.out.println("4");
+	System.out.println("5");
+	}
+	
+}
+
+
+
